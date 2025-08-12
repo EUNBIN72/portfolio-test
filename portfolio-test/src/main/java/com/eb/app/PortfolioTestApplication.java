5aab486419a7e7e18b3c81d82a268730b80bb434
eunbin <kozilee00@gmail.com>
2025-08-12 20:29:10 +0900

    commit

---
+++ portfolio-test/src/main/java/com/eb/app/PortfolioTestApplication.java
@@ -0,0 +1,13 @@
+package com.eb.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioTestApplication.class, args);
+	}
+
+}
---
+++ portfolio-test/src/main/java/com/eb/app/home/HomeController.java
@@ -0,0 +1,16 @@
+package com.eb.app.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		System.out.println("home");
+		return "index";
+	}
+
+}
